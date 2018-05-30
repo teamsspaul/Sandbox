2eed6cdfc6b34a5de39febe925370d13b4cfc11e
Paul Mendoza <paul.m.mendoza@gmail.com>
2018-05-30 14:17:49 -0600

    Commitment

--- PolyFit/MyPro.py
+++ PolyFit/MyPro.py
@@ -3,22 +3,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Data for plotting
-GateWidth = [18,20,22,24,26,30,34,40,50]
-GateWidth = np.array(GateWidth)
-Uncertainty = np.zeros((len(GateWidth),1))
-for i in range(0,len(GateWidth)):
-    Uncertainty[i,0] = np.exp(-GateWidth[i])+0.00000000001*GateWidth[i]**2
+with open('Input.txt') as f:
+    content = f.readlines()
 
+GateWidth = np.zeros((len(content)))
+Uncertainty = np.zeros((len(content)))
+for i in range(0,len(content)):
+    line = content[i].split("\n")[0]
+    GateWidth[i] = float(line.split(",")[0])
+    Uncertainty[i] = float(line.split(",")[1])
+
+#Sort the data
+print(GateWidth)
+print(Uncertainty)
+GateWidth,Uncertainty = (list(t) for t in zip(*sorted(zip(GateWidth,Uncertainty))))
+print(GateWidth)
+print(Uncertainty)
+    
 def MatPrint(Mat,rows,cols):
     s = "";
     for i in range(0,cols):
       for j in range(0,rows):
-        s += "%.2f" % Mat[i,j] + " ";
+        s += "%.2f" % Mat[j,i] + " ";
       s += "\n";
     return s;
-
-print(GateWidth)
-print(Uncertainty)
 
 Order = 9
 #Fit to a polynomial
@@ -40,14 +48,14 @@ for row in range(0,Rows):
 print(MatPrint(XT,Cols,Rows))
 
 a = np.matmul(np.matmul(np.linalg.inv(np.matmul(XT,X)),XT),Uncertainty)
-aRev = np.zeros((len(GateWidth),1))
-for i in range(0,len(GateWidth)):
-    aRev[i] = a[len(GateWidth)-i-1]
+#aRev = np.zeros((len(GateWidth),1))
+#for i in range(0,len(GateWidth)):
+#    aRev[i] = a[len(GateWidth)-i-1]
 
 fig, ax = plt.subplots()
 ax.plot(GateWidth, Uncertainty,'b')
 ax.plot(GateWidth, Y,'k')
-ax.plot(GateWidth, np.polyval(aRev,GateWidth),'g')
+#ax.plot(GateWidth, np.polyval(aRev,GateWidth),'g')
 
 ax.set(xlabel='Gate Width', ylabel='Percent Uncertainty')
 ax.grid()
